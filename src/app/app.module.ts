722c2a294c2c534ba181f87f7e88b8d500e2e114
RyamSousa <ryam.sousa123@gmail.com>
2022-09-30 09:00:28 -0300

    :sparkles: created 'photo-frame' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LikeWidgetModule } from "./shared/components/like-widget/like-widget.module";
+import { PhotoFrameModule } from "./shared/components/photo-frame/photo-frame.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LikeWidgetModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, LikeWidgetModule, PhotoFrameModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/components/photo-frame/photo-frame.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { LikeWidgetModule } from "../like-widget/like-widget.module";
+import { PhotoFrameComponent } from "./photo-frame.component";
+
+@NgModule({
+	imports: [CommonModule, LikeWidgetModule],
+	exports: [PhotoFrameComponent],
+	declarations: [PhotoFrameComponent],
+	providers: [],
+})
+export class PhotoFrameModule {}
